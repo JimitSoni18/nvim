4e6e71a296fb1466248e14e8a7a06a496fd8c8be
jimit <jimitsoni18@gmail.com>
2024-07-10 13:06:29 +0530

    colored my pencils

--- lua/jimit/init.lua
+++ lua/jimit/init.lua
@@ -3,3 +3,8 @@ require("jimit.remap")
 require("jimit.lazy-init")
 
 print("Hello from jimit")
+
+function ColorMyPencils()
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
