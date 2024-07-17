3acaf64e6e7566318eacd574499fec6bdf179037
jimit <jimitsoni18@gmail.com>
2024-07-17 10:59:14 +0530

    removed unused function

--- lua/jimit/init.lua
+++ lua/jimit/init.lua
@@ -3,8 +3,3 @@ require("jimit.remap")
 require("jimit.lazy-init")
 
 print("Hello from jimit")
-
-function ColorMyPencils()
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
