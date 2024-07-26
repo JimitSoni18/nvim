ea7dab9c46a0870f380b7321ff5c441bfc967615
jimit <jimitsoni18@gmail.com>
2024-07-26 12:09:36 +0530

    gitsigns hunk nav, back to onedark

--- lua/jimit/lazy/gitsigns.lua
+++ lua/jimit/lazy/gitsigns.lua
@@ -1,6 +1,17 @@
 return {
 	'lewis6991/gitsigns.nvim',
 	config = function()
-		require('gitsigns').setup()
-	end
+		local gitsigns = require('gitsigns')
+		gitsigns.setup({
+			on_attach = function ()
+				vim.keymap.set('n', ']g', function()
+					gitsigns.nav_hunk('next')
+				end)
+				vim.keymap.set('n', '[g', function()
+					gitsigns.nav_hunk('prev')
+				end)
+			end
+		})
+	end,
+	event = 'BufEnter'
 }
